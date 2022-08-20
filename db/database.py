b8120d04c02f4d95793347b7372960f027d05888
LamequeFernandes <lamequesao@gmail.com>
2022-08-20 17:32:51 -0300

    adicao das configuracoes de banco. #4
    Co-Authored-by: deivid-a1 <deivid_506_02@hotmail.com>
    Co-Authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

---
+++ db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
+
+from db.config import settings
+
+
+engine: AsyncEngine = create_async_engine(settings.DB_URL)
+
+Session: AsyncSession = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+async def get_session():
+    session: AsyncSession = Session()
+
+    try:
+        yield session
+    finally:
+        await session.close()
