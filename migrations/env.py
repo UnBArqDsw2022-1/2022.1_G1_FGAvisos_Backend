4a2abebaf64516cdc3af6d633af8d678a494c1fa
LamequeFernandes <lamequesao@gmail.com>
2022-08-23 21:05:33 -0300

    correcao do dockerfile e adicao das models no alembic. #4

--- migrations/env.py
+++ migrations/env.py
@@ -19,6 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from db.config import settings
+import src.models.__all_models
 target_metadata = settings.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
