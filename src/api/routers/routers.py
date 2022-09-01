44cf2730accc8eb42204e91d1e44fd413761039d
LamequeFernandes <lamequesao@gmail.com>
2022-09-01 16:43:13 -0300

    adicao de rota para turmas.
    Co-Authored-by: JoaoP-Coelho <japapauloxd@hotmail.com>

--- src/api/routers/routers.py
+++ src/api/routers/routers.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
 from src.api.endpoints import professor
+from src.api.endpoints import turma
 
 
 api_router = APIRouter()
 api_router.include_router(professor.router, 
                           prefix='/professor', 
                           tags=['professor'])
+api_router.include_router(turma.router, 
+                          prefix='/turma', 
+                          tags=['turma'])
