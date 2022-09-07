608ed456c1ad70824fb752b7094cd4183e0b17f7
LamequeFernandes <lamequesao@gmail.com>
2022-09-07 02:31:52 -0300

    GETs, PUT, POST e DELETE de avisos. E adicao nas rotas

--- app/api/routers/routers.py
+++ app/api/routers/routers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.api.endpoints import professor
 from app.api.endpoints import turma
+from app.api.endpoints import aviso
 
 
 api_router = APIRouter()
-api_router.include_router(professor.router, prefix='/professor', tags=['professor'])
-api_router.include_router(turma.router, prefix='/turma',tags=['turma'])
+api_router.include_router(professor.router, prefix='/professor', tags=['Professor'])
+api_router.include_router(turma.router, prefix='/turma',tags=['Turma'])
+api_router.include_router(aviso.router, prefix='/aviso',tags=['Aviso'])
