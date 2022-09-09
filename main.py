4a7c7c17e905ba73f6f86fd17f65d296a671b27a
LamequeFernandes <lamequesao@gmail.com>
2022-09-09 08:53:54 -0300

    adicao dos cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routers.routers import api_router
 
@@ -6,6 +7,13 @@ from app.api.routers.routers import api_router
 app = FastAPI(title="FGAvisos API")
 app.include_router(api_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
     import uvicorn
